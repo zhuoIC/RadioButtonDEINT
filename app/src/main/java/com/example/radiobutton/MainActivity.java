b2bd587ed8f2914e63260764c0e85ebd45eebdab
zhuoIC <nicolimonar@gmail.com>
2017-12-01 14:20:24 +0100

    RadioGroup con CheckedChange

--- app/src/main/java/com/example/radiobutton/MainActivity.java
+++ app/src/main/java/com/example/radiobutton/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 /**
  * Clase de ejemplo de cómo funciona RadioButton
@@ -13,19 +15,34 @@ import android.view.View;
 public class MainActivity extends AppCompatActivity{
     private View contraintParticular;
     private View contraintBussiness;
+    private RadioGroup radioGroup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         contraintParticular = findViewById(R.id.constraintParticular);
         contraintBussiness = findViewById(R.id.constraintBussiness);
+        radioGroup = findViewById(R.id.rdgTypeClient);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
+                    case R.id.rbtBussiness:
+                        viewParticular(false);
+                        break;
+                    case R.id.rbtParticular:
+                        viewParticular(true);
+                        break;
+                }
+            }
+        });
     }
 
     /**
      * Método que gestiona el evento onClick en los componentes RadioButton de la Interface
      * @param view
      */
-    public void onRadioButtonClicked(View view) {
+    /*public void onRadioButtonClicked(View view) {
         switch (view.getId()) {
             case R.id.rbtParticular:
                 viewParticular(true);
@@ -34,7 +51,7 @@ public class MainActivity extends AppCompatActivity{
                 viewParticular(false);
                 break;
         }
-    }
+    }*/
 
     /**
      * Método que indica qué ConstraintLayout ha de estar visible según la opción seleccionada
